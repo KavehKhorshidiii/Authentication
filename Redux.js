f6aa96453a505779ba6d18a0bbfd98ef60bcbe30
Kaveh <kavehkhorshidiii@gmail.com>
2026-05-23 23:18:23 +0330

    commit

--- Redux.js
+++
@@ -1,7 +0,0 @@
-// let products = [
-//     ["pen" , "pencile" , "book"],
-//     ["phone" , "laptop" , "airpod"],
-//     ["car" , "bysicle" , "motor"],
-//     ["ali" , "reza" , "mashala"]
-// ]
-
