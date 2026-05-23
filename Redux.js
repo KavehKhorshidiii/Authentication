e46719be181642fb413c36c6222e5e5046c8c7b5
Kaveh <kavehkhorshidiii@gmail.com>
2026-05-23 23:17:54 +0330

    commit

--- Redux.js
+++ Redux.js
@@ -13,12 +13,4 @@
 // console.log(myBox)
 
 
-
-let numbers = [1,2,3,4,5,6]
-
-const box = numbers.reduce((prevNum , nextNum)=>{
-    console.log(prevNum , nextNum)
-    return prevNum + nextNum
-})
-
 console.log(box)
