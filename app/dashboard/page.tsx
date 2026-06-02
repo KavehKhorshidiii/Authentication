5cc59a0c165bb9483a336ef78fa2c6b4b506ddb2
Kaveh <kavehkhorshidiii@gmail.com>
2026-06-02 20:43:04 +0330

    commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { verifyToken } from '@/utils/auth'
+import { userModel } from '@/models/userModel';
 
 export default async function dashboard() {
 
@@ -9,16 +10,19 @@ export default async function dashboard() {
 
 
     const isVerifyToken = verifyToken(myToken)
-   
 
-    if(!myToken || !isVerifyToken){
+
+    if (!myToken || !isVerifyToken) {
         redirect("/")
     }
+
+    const userData = await userModel.findOne({ $or: [{ username: isVerifyToken.usernameOrEmail }, { email: isVerifyToken.usernameOrEmail }, { email: isVerifyToken.email }] }).select("firstname")
 
     
     return (
         <div>
-            Dashboard
+            <p>Dashboard</p>
+            <p>Welcome {userData.firstname}</p>
         </div>
     )
 }
