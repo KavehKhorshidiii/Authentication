eecd903080766893861dd25ab8ca75d73d69ab75
Kaveh <kavehkhorshidiii@gmail.com>
2026-06-02 22:00:05 +0330

    commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -22,10 +22,11 @@ export default async function Dashboard() {
     }
 
 
+    
     const userData = await userModel.findById(isVerifyToken.userID).select("firstname")
 
 
-    
+
     return (
         <div>
             <p>Dashboard</p>
